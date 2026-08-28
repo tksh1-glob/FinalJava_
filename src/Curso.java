c964715759c1d04038dcd08b8b64c47c21955cf0
tksh1-glob <gustavo.cabrera@globant.com>
2026-08-27 21:32:56 -0500

    Curso

---
+++ src/Curso.java
@@ -0,0 +1,65 @@
+package com.universidad.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curso {
+
+    private static int contadorCursos = 0;
+
+    private final int id;
+    private String nombre;
+    private String salon;
+    private Profesor profesor;
+    private final List<Estudiante> estudiantes;
+
+    public Curso(String nombre, String salon, Profesor profesor) {
+        this.id = ++contadorCursos;
+        this.nombre = nombre;
+        this.salon = salon;
+        this.profesor = profesor;
+        this.estudiantes = new ArrayList<>();
+    }
+
+    public Curso(String nombre, String salon, Profesor profesor, List<Estudiante> estudiantes) {
+        this(nombre, salon, profesor);
+        this.estudiantes.addAll(estudiantes);
+    }
+
+    public static int getContadorCursos() {
+        return contadorCursos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSalon() {
+        return salon;
+    }
+
+    public void setSalon(String salon) {
+        this.salon = salon;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public List<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+    
+}
